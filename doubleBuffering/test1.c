fc6adc3c76063314818a410a3629a6447074eba3
Danilo Vieria Costa <danilo.11235@outlook.com>
2026-04-22 22:56:19 -0300

    Show the concept of double buffering to graphics and creating a text window using WM_CHAR and a struct-based iterator.

--- doubleBuffering/test1.c
+++ doubleBuffering/test1.c
@@ -5,6 +5,13 @@
 #define SIDESQUARE 50
 
 // global variables
+typedef struct
+{
+    size_t size;
+    size_t limitOfCharacters;
+    wchar_t * buffer;
+} TextGenerator;
+
 HBRUSH wndBrush = {0};
 HBRUSH squareBrush = {0};
 HDC backDC = NULL;
@@ -13,6 +20,11 @@ int bufferWidth;
 int bufferHeight;
 int mouseX;
 int mouseY;
+wchar_t bufferText[7680] = {0};
+TextGenerator bufferTextGenerator = {0, 7680, bufferText};
+
+int add2Text(wchar_t c, TextGenerator * state);
+int backspace(TextGenerator * state);
 
 LRESULT CALLBACK renderProc(HWND hwnd, UINT nMsg, WPARAM wParam, LPARAM lParam);
 
@@ -57,7 +69,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR argvW, i
         }
 
         InvalidateRect(hwnd, NULL, FALSE); 
-        Sleep(1);        
+        Sleep(16);        
     }
     
 
@@ -87,14 +99,17 @@ LRESULT CALLBACK renderProc(HWND hwnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
             PAINTSTRUCT ps = {0};
             HDC hdc = BeginPaint(hwnd, &ps);
             
-            wndBrush = CreateSolidBrush(RGB(255, 0,0));
+            //wndBrush = CreateSolidBrush(RGB(255, 0,0));
             RECT rect = {0, 0, bufferWidth, bufferHeight};
-            FillRect(backDC, &rect, wndBrush);
+            FillRect(backDC, &rect, (HBRUSH)(COLOR_WINDOW+1));
+
+            TextOutW(backDC, 50, 50, bufferText, lstrlenW(bufferText));
 
             RECT mouseSquare = {mouseX, mouseY, mouseX + SIDESQUARE, mouseY + SIDESQUARE};
             COLORREF squareColor = RGB((((mouseX % 256) + 256) % 256), ((((mouseX + mouseX)/2) % 256) + 256) % 256, (((mouseY % 256) + 256) % 256));
             squareBrush = CreateSolidBrush(squareColor);
             FillRect(backDC, &mouseSquare, squareBrush);
+            DeleteObject(squareBrush);
 
             BitBlt(hdc, 0, 0, bufferWidth, bufferHeight, backDC, 0, 0, SRCCOPY);
             
@@ -130,6 +145,21 @@ LRESULT CALLBACK renderProc(HWND hwnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
         {
             mouseX = (int)(short)LOWORD(lParam);
             mouseY = (int)(short)HIWORD(lParam);
+            return 0;
+        }
+
+        case WM_CHAR:
+        {
+            if (wParam == '\b')
+            {
+                backspace(&bufferTextGenerator);
+                return 0;
+            }
+            
+            wchar_t c = (wchar_t)wParam;
+            add2Text(c, &bufferTextGenerator);
+
+            return 0;
         }
         
         default:
@@ -137,4 +167,34 @@ LRESULT CALLBACK renderProc(HWND hwnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
     }
 
     return DefWindowProc(hwnd, nMsg, wParam, lParam);
+}
+
+int add2Text(wchar_t c, TextGenerator * state)
+{
+    size_t size = state->size;
+    if (size < state->limitOfCharacters - 1)
+    {
+        state->buffer[size++] = c;
+        state->buffer[size] = L'\0';
+
+        state->size = size;
+
+        return 0;
+    }
+
+    MessageBoxW(NULL, L"Stop typing! There is no more memory for this", L"Error", MB_OK);
+    return -1;   
+}
+int backspace(TextGenerator * state)
+{
+    if (state->size <=0)
+    {
+        MessageBoxW(NULL, L"Which text do you want to erase, moron? Give me a break.", L"Error", MB_OK);
+        return -1;
+    }
+    
+    state->buffer[state->size - 1] = L'\0';
+    state->size--;
+
+    return 0;
 }
